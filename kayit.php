160a99c2cbe615043ffea05d576200f2dc8dc53a
RecepDemirel07 <recep.demirel2@ogr.sakarya.edu.tr>
2024-05-12 23:51:06 +0300

    Şehrim sayfası ve cv sayfasına eklemeler yapıldı

--- kayit.php
+++ kayit.php
@@ -1,8 +1,38 @@
+<?php
+    if($_POST["student-no"] == "b231210023" && $_POST["password"] == "b231210023") {
+        echo "
+        <html>
+        <head>
+            <style>
+            
+            .success-message {
+                background-color: #dff0d8;
+                border: 1px solid #d6e9c6;
+                color: #4CAF50;
+                padding: 15px;
+                margin: 20px auto;
+                max-width: 400px;
+                border-radius: 5px;
+            }
+            
+            .success-message h2 {
+                font-size: 24px;
+                margin-top: 0;
+            }
+            
+            .success-message p {
+                font-size: 18px;
+                margin-bottom: 0;
+            }</style>
+        </head>
+        <body>
+        <div class = 'success-message'>
 
- <?php
-        if($_POST["student-no"] == "b231210023" && $_POST["password"] == "b231210023") {
-            echo "B231210023 Giriş Yaptın";
-            header("refresh:2;url=Anasayfa.html");
-            exit();
-        }
- ?>
+             <h3>b231210023 Hoşgeldiniz</h3> 
+        </div>
+        </body>
+        </html>";
+        header("refresh:2;url=Anasayfa.html");
+        exit();
+    }
+?>
